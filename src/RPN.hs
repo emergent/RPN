24fe9673218bb61777459af7afbf1253bf6e4207
emergent <yosikawa@gmail.com>
2018-03-21 04:37:39 +0900

    concerned error case

--- src/RPN.hs
+++ src/RPN.hs
@@ -1,13 +1,24 @@
-module RPN (solveRPN) where
+module RPN where
 
-solveRPN :: String -> Double
-solveRPN = head . foldl foldingFunction [] . words
-    where
-        foldingFunction (x:y:ys) "*" = (y * x):ys
-        foldingFunction (x:y:ys) "+" = (y + x):ys
-        foldingFunction (x:y:ys) "-" = (y - x):ys
-        foldingFunction (x:y:ys) "/" = (y / x):ys
-        foldingFunction (x:y:ys) "^" = (y ** x):ys
-        foldingFunction (x:xs) "ln" = log x:xs
-        foldingFunction xs "sum" = [sum xs]
-        foldingFunction xs numberString = read numberString:xs
+import Data.List
+import Control.Monad
+
+solveRPN :: String -> Maybe Double
+solveRPN st = do
+    [result] <- foldM foldingFunction [] (words st)
+    return result
+
+foldingFunction :: [Double] -> String -> Maybe [Double]
+foldingFunction (x:y:ys) "*" = return ((y * x):ys)
+foldingFunction (x:y:ys) "+" = return ((y + x):ys)
+foldingFunction (x:y:ys) "-" = return ((y - x):ys)
+foldingFunction (x:y:ys) "/" = return ((y / x):ys)
+foldingFunction (x:y:ys) "^" = return ((y ** x):ys)
+foldingFunction (x:xs) "ln" = return (log x:xs)
+foldingFunction xs "sum" = return [sum xs]
+foldingFunction xs "product" = return [product xs]
+foldingFunction xs numberString = fmap (:xs) (readMaybe numberString)
+
+readMaybe :: (Read a) => String -> Maybe a
+readMaybe st = case reads st of [(x, "")] -> Just x
+                                _ -> Nothing
--- test/RPNSpec.hs
+++ test/RPNSpec.hs
@@ -7,37 +7,49 @@ spec :: Spec
 spec = do
     describe "solveRPN" $ do
         it "case 1-01" $ do
-            solveRPN "10 4 3 + 2 * -" `shouldBe` -4.0
+            solveRPN "10 4 3 + 2 * -" `shouldBe` Just (-4.0)
 
     describe "solveRPN" $ do
         it "case 1-02" $ do
-            solveRPN "2 3.5 +" `shouldBe` 5.5
+            solveRPN "2 3.5 +" `shouldBe` Just 5.5
 
     describe "solveRPN" $ do
         it "case 1-03" $ do
-            solveRPN "90 34 12 33 55 66 + * - +" `shouldBe` -3947.0
+            solveRPN "90 34 12 33 55 66 + * - +" `shouldBe` Nothing
 
     describe "solveRPN" $ do
         it "case 1-04" $ do
-            solveRPN "90 34 12 33 55 66 + * - + -" `shouldBe` 4037.0
+            solveRPN "90 34 12 33 55 66 + * - + -" `shouldBe` Just 4037.0
 
     describe "solveRPN" $ do
         it "case 1-05" $ do
-            solveRPN "90 3.8 -" `shouldBe` 86.2
+            solveRPN "90 3.8 -" `shouldBe` Just 86.2
                     
     describe "solveRPN - extension" $ do
         it "case 2-01" $ do
-            solveRPN "2.7 ln" `shouldBe` (log 2.7)
+            solveRPN "2.7 ln" `shouldBe` Just (log 2.7)
                     
     describe "solveRPN - extension" $ do
         it "case 2-02" $ do
-            solveRPN "10 10 10 10 sum 4 /" `shouldBe` 10.0
+            solveRPN "10 10 10 10 sum 4 /" `shouldBe` Just 10.0
                     
     describe "solveRPN - extension" $ do
         it "case 2-03" $ do
-            solveRPN "10 10 10 10 10 sum 4 /" `shouldBe` 12.5
+            solveRPN "10 10 10 10 10 sum 4 /" `shouldBe` Just 12.5
                     
     describe "solveRPN - extension" $ do
         it "case 2-04" $ do
-            solveRPN "10 2 ^" `shouldBe` 100.0
-                            
+            solveRPN "10 2 ^" `shouldBe` Just 100.0
+                    
+    describe "solveRPN - extension" $ do
+        it "case 2-05" $ do
+            solveRPN "10 4 4 product 16 /" `shouldBe` Just 10.0
+
+    describe "solveRPN - error" $ do
+        it "case 3-01" $ do
+            solveRPN "go to hell" `shouldBe` Nothing
+
+    describe "solveRPN - error" $ do
+        it "case 3-02" $ do
+            solveRPN "1 wawawa" `shouldBe` Nothing
+
